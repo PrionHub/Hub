4149ff6a638f0388902dfb80a13d39341b352578
Prion <personalcom969@gmail.com>
2026-07-15 13:19:46 -0500

    Create Section.lua

---
+++ Modules/UI/Section.lua
@@ -0,0 +1,19 @@
+local Section = {}
+
+Section.__index = Section
+
+function Section.new(Name)
+
+    local self = setmetatable({}, Section)
+
+    self.Name = Name
+
+    self.Container = nil
+
+    self.Elements = {}
+
+    return self
+
+end
+
+return Section
