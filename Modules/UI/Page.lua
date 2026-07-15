ca1c8c79db30a705fcf650cb7010fd68dd2d8bdf
Prion <personalcom969@gmail.com>
2026-07-15 12:45:35 -0500

    Update Page.lua

--- Modules/UI/Page.lua
+++ Modules/UI/Page.lua
@@ -1,4 +1,5 @@
 local Page = {}
+
 Page.__index = Page
 
 function Page.new(Name)
@@ -6,10 +7,34 @@ function Page.new(Name)
     local self = setmetatable({}, Page)
 
     self.Name = Name
-    self.Frame = nil
-    self.Sections = {}
+
+    self.Container = nil
+
+    self.Elements = {}
+
+    self.Visible = false
 
     return self
+
+end
+
+function Page:Show()
+
+    self.Visible = true
+
+    if self.Container then
+        self.Container.Visible = true
+    end
+
+end
+
+function Page:Hide()
+
+    self.Visible = false
+
+    if self.Container then
+        self.Container.Visible = false
+    end
 
 end
 
