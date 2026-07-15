8f8dad6b31cf83f679a68db69f27c2a8086ee205
Prion <personalcom969@gmail.com>
2026-07-14 19:10:49 -0500

    Create Animation.lua

---
+++ Modules/Utils/Animation.lua
@@ -0,0 +1,41 @@
+local Animation = {}
+
+local TweenService = game:GetService("TweenService")
+
+Animation.Info = {
+
+    Fast = TweenInfo.new(
+        0.12,
+        Enum.EasingStyle.Quad,
+        Enum.EasingDirection.Out
+    ),
+
+    Normal = TweenInfo.new(
+        0.20,
+        Enum.EasingStyle.Quad,
+        Enum.EasingDirection.Out
+    ),
+
+    Slow = TweenInfo.new(
+        0.35,
+        Enum.EasingStyle.Quad,
+        Enum.EasingDirection.Out
+    )
+
+}
+
+function Animation.Tween(Object, Info, Properties)
+
+    local Tween = TweenService:Create(
+        Object,
+        Info,
+        Properties
+    )
+
+    Tween:Play()
+
+    return Tween
+
+end
+
+return Animation
