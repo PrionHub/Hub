272923ede29233c10840b1c0212ad0d833c9fd78
Prion <personalcom969@gmail.com>
2026-07-15 13:04:09 -0500

    Update Hub.lua

--- Modules/Core/Hub.lua
+++ Modules/Core/Hub.lua
@@ -1,12 +1,14 @@
 local Hub = {}
 local UI
 local Page
+local Components
 
 Hub.Pages = {}
 
 function Hub.Init(App)
 
     Page = App.Page
+    Components = App.Components
 
 end
 
@@ -19,6 +21,22 @@ end
 function Hub:CreatePage(Name)
 
     local NewPage = Page.new(Name)
+
+    local Container = Components.CreateFrame({
+
+        Name = Name,
+
+        Size = UDim2.fromScale(1,1),
+
+        BackgroundTransparency = 1,
+
+        Visible = false,
+
+        Parent = UI.Content
+
+    })
+
+    NewPage.Container = Container
 
     table.insert(self.Pages, NewPage)
 
